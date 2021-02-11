fa437eac2f2b314a317cd3cc0733b9d08777dea1
JNJER <66479275+JNJER@users.noreply.github.com>
2021-02-11 17:01:32 +0100

    patch & notebook

---
+++ detect_data_classif.py
@@ -0,0 +1,32 @@
+
+from DCNN_dataset_maker.models import *
+
+i = 0 
+suppress=None
+
+for i_image, (data, label) in enumerate(image_dataset_SSD):
+    
+        # Forward prop.
+        predicted_locs, predicted_scores = model(data.unsqueeze(0).to(device)) # Move to default device
+
+        # Detect objects in SSD output
+        det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
+                                                                 max_overlap=max_overlap, top_k=top_k)
+
+        # Decode class integer labels
+        det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+
+        # Sort by the labels
+        is_anime = False
+        for lab_ in det_labels :
+            if lab_ in animated:
+                is_anime = True
+                
+        if is_anime : 
+            i+=1
+            print(image_dataset.imgs[i_image][0], is_anime)
+            shutil.copy(image_dataset.imgs[i_image][0], path_out_a)
+        else:
+            print(image_dataset.imgs[i_image][0], is_anime)
+            shutil.copy(image_dataset.imgs[i_image][0], path_out_b)
+print(i)
